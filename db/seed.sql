132514cba1c90eefadd12e6d874cf7881103d861
LPMartinez <lpmartinez1109@gmail.com>
2021-03-17 21:56:18 -0500

    added database information for mysql

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+use marvel;
+
+INSERT INTO team
+    (name)
+VALUES
+("Avengers"),
+("Guardians Of The Galaxy"),
+("Fantastic Four"),
+("X-Men");
+
+INSERT INTO hero
+    (first_name, last_name, code_name, team_id)
+VALUES
+    ("Steve", "Rogers", "Captain America", 1)
